3769c3e1c360db8c649687a4d17d9d2c572b7098
Fletcher Kelly <fletcher_kelly@live.co.za>
2023-07-25 15:04:02 +0100

    Update az_listSubscriptionsandConnect.ps1

--- subscriptions/az/az_listSubscriptionsandConnect.ps1
+++ subscriptions/az/az_listSubscriptionsandConnect.ps1
@@ -1,12 +1,20 @@
 Login-AzAccount
 $subscriptions = Get-AzSubscription | Sort-Object SubscriptionName | Select-Object Name,SubscriptionId
 [int]$subscriptionCount = $subscriptions.count
-Write-Host "Found" $subscriptionCount "Subscriptions"
+Write-output "Found" $subscriptionCount "Subscriptions"
+
+# Check if there are any subscriptions found
+if ($subscriptionCount -eq 0) {
+  Write-Output "No subscriptions found. Please ensure you have access to at least one Azure subscription."
+  return
+}
+
 $i = 0
-foreach ($subscription in $subscriptions)
+foreach ($subscription in $subscriptions | Sort-Object -Property Name )
 {
   $subValue = $i
-  Write-Host $subValue ":" $subscription.Name "("$subscription.SubscriptionId")"
+  $subText = [string]$subValue + " : " + $subscription.Name + " ( " + $subscription.SubscriptionId + " ) "
+  Write-output $subText
   $i++
 }
 Do 
@@ -15,5 +23,6 @@ Do
 } 
 until ($subscriptionChoice -le $subscriptionCount)
 
-Write-Host "You selected" $subscriptions[$subscriptionChoice].Name
+$selectedSub = "You selected " + $subscriptions[$subscriptionChoice].Name
+Write-output $selectedSub
 Set-AzContext -SubscriptionId $subscriptions[$subscriptionChoice].SubscriptionId
