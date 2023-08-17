798201169b740117a2194ba8c7c76c26c89ffd6d
Fletcher Kelly <fletcher_kelly@outlook.com>
2023-08-17 09:05:42 +0100

    new auth scripts

---
+++ authentication/get-token-v2.ps1
@@ -0,0 +1,21 @@
+$tenantId = Read-Host "Enter your Azure tenant ID"
+$clientID = Read-Host "Enter your Azure client ID"
+$clientSecret = Read-host "enter your client secret" -AsSecureString
+
+$normalString = [System.Runtime.InteropServices.Marshal]::PtrToStringAuto([System.Runtime.InteropServices.Marshal]::SecureStringToBSTR($clientSecret))
+
+$params = @{
+    Uri = "https://login.microsoftonline.com/$tenantId/oauth2/v2.0/token"
+    Method = "POST"
+    Body = @{
+        grant_type = "client_credentials"
+        scope = "https://management.azure.com/.default"
+        client_id = $clientID
+        client_secret = $normalString
+    }
+}
+
+$authResult = Invoke-RestMethod @params
+
+# Output the access token
+$token = $authResult.access_token
